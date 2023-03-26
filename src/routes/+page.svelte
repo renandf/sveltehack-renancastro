2155727cf8aaefd9280eb732be9614aeda720d67
Renan Castro <renandf@gmail.com>
2023-03-26 11:11:20 +1100

    feat: :heavy_plus_sign: Add DaisyUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <h1 class="text-3xl font-bold underline">Hello world!</h1>
 
+<button class="btn">Button</button>
+
 <style lang="postcss">
 	:global(html) {
 		background-color: theme(colors.gray.100);
